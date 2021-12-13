dfe1cea33e8f0ffbd4652bd6b3166be71c3cfd5d
bala <bala@us.ibm.com>
2021-12-13 12:38:02 -0600

    added print outs

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -13,20 +13,21 @@ CHART_DIR=$(cd "${MODULE_DIR}"; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
 
+
+echo "${DEST_DIR}"
+echo "${CHART_DIR}"
+echo "Values_content"
+
 ## Add logic here to put the yaml resource content in DEST_DIR
 
 find "${DEST_DIR}" -name "*"
 
-DEST_DIR="$1"
 SERVER_VALUES_FILE="$2"
 
 mkdir -p "${DEST_DIR}"
 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
 
- echo "${DEST_DIR}"
-  echo "${CHART_DIR}"
-  echo "Values_content"
 
 if [[ -n "${VALUES_CONTENT}" ]]; then
   echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
