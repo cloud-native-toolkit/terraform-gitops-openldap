08c671b39ea9047788e02cca0d04e01d75902172
bala <bala@us.ibm.com>
2021-12-14 11:23:01 -0600

    debugging

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -6,6 +6,9 @@ MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
 CHART_DIR=$(cd "${MODULE_DIR}"; pwd -P)
 
+echo "2"
+echo "{2}"
+
 NAME="$1"
 DEST_DIR="$2"
 
@@ -31,7 +34,6 @@ SERVER_VALUES_FILE="$2"
 mkdir -p "${DEST_DIR}"
 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
-
 
 if [[ -n "${VALUES_CONTENT}" ]]; then
   echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
