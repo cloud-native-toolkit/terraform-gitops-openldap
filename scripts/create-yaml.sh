bea441abaf225cc665d16cfa7d43bd7ea820e899
bala <bala@us.ibm.com>
2022-01-07 10:29:06 -0600

    Values commented

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -28,9 +28,9 @@ mkdir -p "${DEST_DIR}"
 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
 
-if [[ -n "${VALUES_CONTENT}" ]]; then
-  echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
-fi
+#if [[ -n "${VALUES_CONTENT}" ]]; then
+#  echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
+#fi
 
 if [[ -n "${VALUES_SERVER_CONTENT}" ]] && [[ -n "${SERVER_VALUES_FILE}" ]]; then
   echo "${VALUES_SERVER_CONTENT}" > "${DEST_DIR}/${SERVER_VALUES_FILE}"
