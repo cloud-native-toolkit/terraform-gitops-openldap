8b13abcf3fcb8787f3a60ef0c2dfe063910c90b1
bala <bala@us.ibm.com>
2022-01-20 09:30:31 -0600

    Clean up cluster dependency

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ locals {
   name          = "openldap"
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
-  ingress_host  = "${local.name}-${var.namespace}.${var.cluster_ingress_hostname}"
-  ingress_url   = "https://${local.ingress_host}"
+  #ingress_host  = "${local.name}-${var.namespace}.${var.cluster_ingress_hostname}"
+  #ingress_url   = "https://${local.ingress_host}"
   service_url   = "http://${local.name}.${var.namespace}"
   
   # OpenLDAP Values.yaml
